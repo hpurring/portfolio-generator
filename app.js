178be0ba0e6c2722b3df6e7c6513499d0d094c4c
hpurring <hilarypurrington@gmail.com>
2021-11-23 22:52:15 -0500

    feature/generate-webpage

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length); 
-console.log(profileDataArgs);
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+const fs = require('fs');
 
-    console.log('==============');
+const profileDataArgs = process.argv.slice(2); 
+
+const [name, github] = profileDataArgs;
+
+
+// console.log(profileDataArgs);
+
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
+
+//     console.log('==============');
     
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
 
-};
+// };
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
+
+
+  
+  
+  fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
